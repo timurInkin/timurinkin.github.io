7e95238a5d45f70d2ea490d5b33df3588b0ef10e
ValievAT <mail123@yandex.ru>
2023-01-26 08:53:25 +0500

    background gradient

--- assets/js/newscript.js
+++ assets/js/newscript.js
@@ -217,3 +217,19 @@ secondTab.addEventListener("click", () => {
   secondSlide.forEach((elem) => elem.style.display = "block")
   toggle.style.left = "50%"
 })
+
+
+let granimInstance = new Granim({
+  element: '#canvas-basic',
+  direction: 'left-right',
+  isPausedWhenNotInView: true,
+  states : {
+      "default-state": {
+          gradients: [
+              ['#ff9966', '#ff5e62'],
+              ['#00F260', '#0575E6'],
+              ['#e1eec3', '#f05053']
+          ]
+      }
+  }
+});
